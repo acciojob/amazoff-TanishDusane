49f5ae812b49f832d62408b3964527598e867885
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-04-09 17:29:06 +0530

    Copied code from Parveen

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -22,3 +22,4 @@ public class DeliveryPartner {
         this.numberOfOrders = numberOfOrders;
     }
 }
+
--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,10 +5,21 @@ public class Order {
     private String id;
     private int deliveryTime;
 
+
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+
+        String[] parts = deliveryTime.split(":");
+
+        // Parse hours and minutes from string to integers
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+
+        // Calculate delivery time in minutes
+        this.deliveryTime = hours * 60 + minutes;
     }
 
     public String getId() {
